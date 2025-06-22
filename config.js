68c125b989411e23d1649483eba3b5c535b4cfc2
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-06-23 00:21:16 +0800

    feat(waline-plugin-uid): :sparkles: add uid cookie when commenting

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-import privacy from "@waline-plugins/privacy";
+import privacy from '@waline-plugins/privacy';
+import pluginUid from 'waline-plugin-uid';
 
 const apiAddress = process.env.CLASS_IT_UP_API;
 
@@ -7,21 +8,21 @@ const config = {
     if (apiAddress) {
       /** @type {{ predicted_label: 0 | 1 }} */
       const res = await fetch(apiAddress, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ content: comment.comment }),
       }).then((res) => res.json());
       if (res.predicted_label === 1) {
-        think.logger.warn("Stalking detected");
-        comment.status = "waiting";
+        think.logger.warn('Stalking detected');
+        comment.status = 'waiting';
         throw new Error(
-          "Stalking-like comment detected, please rearrange your comment."
+          'Stalking-like comment detected, please rearrange your comment.',
         );
       }
     }
   },
 
-  plugins: [privacy],
+  plugins: [privacy, pluginUid],
 };
 
 export default config;
---
+++ packages/waline-plugin-uid/src/index.ts
@@ -0,0 +1,45 @@
+import jwt from '@node-rs/jsonwebtoken';
+import type { Middleware } from 'koa';
+import bannedUids from '../../../data/banned-uids.json' with { type: 'json' };
+
+if (!process.env.JWT_TOKEN)
+  throw new Error(
+    'JWT_TOKEN is not set in environment variables. Please set it to use the UID plugin.',
+  );
+
+const jwtToken = process.env.JWT_TOKEN;
+
+const addCookie: Middleware = async (ctx, next) => {
+  if (ctx.path !== '/api/comment' || ctx.method.toUpperCase() !== 'POST')
+    return next();
+  let uid: string | undefined;
+  const uidCookie = ctx.cookies.get('uid');
+  if (uidCookie) {
+    try {
+      uid = (await jwt.verify(uidCookie, jwtToken)).uid;
+    } catch (e) {
+      think.logger.error(`Invalid UID token: ${e}`);
+    }
+  }
+  if (!uid) {
+    const newUid = crypto.randomUUID();
+    const exp = Date.now() + 365 * 24 * 60 * 60 * 1000; // 1 year
+    const token = await jwt.sign({ uid: newUid, exp }, jwtToken);
+    uid = newUid;
+    ctx.cookies.set('uid', token, {
+      sameSite: 'strict',
+      path: '/api/comment',
+      expires: new Date(exp),
+    });
+  }
+  if (bannedUids.includes(uid)) {
+    ctx.status = 403;
+    ctx.body = { errno: 403, errmsg: 'Your UID is banned from commenting.' };
+    think.logger.warn(`Banned UID attempted to comment: ${uid}`);
+    return;
+  }
+  think.logger.info(`New comment from UID: ${uid}`);
+  await next();
+};
+
+export default { middlewares: [addCookie] };
